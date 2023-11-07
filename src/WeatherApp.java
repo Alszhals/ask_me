243869b1860599bb0248d7e437c0fda1d14efffd
LAB1 ADS <asek05052005@gmail.com>
2023-11-08 02:25:25 +0600

    ask

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Scanner;
@@ -6,6 +8,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Stack;
 
 // Strategy Pattern: WeatherDataCollectionStrategy
 interface WeatherDataCollectionStrategy {
@@ -132,11 +135,14 @@ class WeatherData extends Observable {
     private static final String URL = "jdbc:postgresql://localhost:5432/sdp";
     private static final String USER = "postgres";
     private static final String PASSWORD = "h1H1h2H2";
-
+    public WeatherConditionState conditionState;
+    public void setConditionState(WeatherConditionState conditionState) {
+        this.conditionState = conditionState;
+    }
     private double temperatureCelsius;
     private double humidity;
     private double pressure;
-    private WeatherConditionState conditionState;
+
 
     private WeatherData() {
         // Private constructor to prevent external instantiation.
@@ -158,7 +164,7 @@ class WeatherData extends Observable {
             conditionState = new SunnyState();
         } else if (temperatureCelsius > 15.0) {
             conditionState = new CloudyState();
-        } else if (temperatureCelsius > 0.0) {
+        } else if( temperatureCelsius > 0.0) {
             conditionState = new RainyState();
         } else {
             conditionState = new SnowyState();
@@ -256,17 +262,68 @@ class WeatherData extends Observable {
     }
 }
 
+class WeatherDataMemento {
+    private double temperatureCelsius;
+    private double humidity;
+    private double pressure;
+    private WeatherConditionState conditionState;
+
+    public WeatherDataMemento(double temperatureCelsius, double humidity, double pressure, WeatherConditionState conditionState) {
+        this.temperatureCelsius = temperatureCelsius;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.conditionState = conditionState;
+    }
+
+    public double getTemperatureCelsius() {
+        return temperatureCelsius;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public WeatherConditionState getConditionState() {
+        return conditionState;
+    }
+}
+
+class DataSourceChange {
+    private WeatherDataCollectionStrategy dataCollectionStrategy;
+    private WeatherDataMemento weatherDataMemento;
+
+    public DataSourceChange(WeatherDataCollectionStrategy strategy, WeatherDataMemento memento) {
+        this.dataCollectionStrategy = strategy;
+        this.weatherDataMemento = memento;
+    }
+
+    public WeatherDataCollectionStrategy getStrategy() {
+        return dataCollectionStrategy;
+    }
+
+    public WeatherDataMemento getMemento() {
+        return weatherDataMemento;
+    }
+}
+
 public class WeatherApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         WeatherData weatherData = WeatherData.getInstance();
         String temperatureScale = "Celsius";
+        WeatherDataCollectionStrategy dataCollectionStrategy = new WeatherAPI(); // Initialize with a default strategy
+
+        Deque<DataSourceChange> dataSourceHistory = new ArrayDeque<>();
+        Deque<String> temperatureScaleHistory = new ArrayDeque<>();
 
         System.out.println("Weather Monitoring and Alert System");
         System.out.println("Enter 'q' to exit.");
 
         while (true) {
-            System.out.println("<=======================Welcome=======================>");
             System.out.println("Menu:");
             System.out.println("1. Collect Weather Data");
             System.out.println("2. Choose Temperature Scale (Celsius/Fahrenheit/Kelvin)");
@@ -274,90 +331,94 @@ public class WeatherApp {
             System.out.println("4. Change Data Source (API/Sensor)");
             System.out.println("5. Display Weather Data");
             System.out.println("6. Display Weather Condition");
-            System.out.println("7. Insert Weather Data into Database");
-            System.out.println("8. Retrieve Weather Data from Database by Time");
+            System.out.println("7.  Weather Data from Database");
+            System.out.println("8. Undo Last Operation");
             System.out.print("Select an option: ");
 
             String choice = scanner.nextLine();
 
             if ("q".equalsIgnoreCase(choice)) {
                 break;
-            } else if ("1".equals(choice)) {
-                System.out.print("Choose Data Source (API/Sensor): ");
+            } else if ("1".equals(choice) || "4".equals(choice)) {
+                if ("1".equals(choice)) {
+                    System.out.print("Choose Data Source (API/Sensor): ");
+                } else {
+                    System.out.print("Change Data Source (API/Sensor): ");
+                }
                 String input = scanner.nextLine();
-                WeatherDataCollectionStrategy dataCollectionStrategy;
 
+                WeatherDataCollectionStrategy newDataSourceStrategy;
                 if ("API".equalsIgnoreCase(input)) {
-                    dataCollectionStrategy = new WeatherAPI();
+                    newDataSourceStrategy = new WeatherAPI();
                 } else if ("Sensor".equalsIgnoreCase(input)) {
-                    dataCollectionStrategy = new WeatherSensor();
+                    newDataSourceStrategy = new WeatherSensor();
                 } else {
                     System.out.println("Invalid choice. Please enter 'API' or 'Sensor'.");
                     continue;
                 }
 
+                // Save the current state to the history
+                DataSourceChange change = new DataSourceChange(dataCollectionStrategy, new WeatherDataMemento(
+                        weatherData.getTemperatureCelsius(), weatherData.getHumidity(), weatherData.getPressure(),
+                        weatherData.conditionState
+                ));
+                dataSourceHistory.push(change);
+
+                dataCollectionStrategy = newDataSourceStrategy;
                 dataCollectionStrategy.collectData(weatherData);
 
-                // After collecting data, add it to the database
-                System.out.print("Enter the time (e.g., '2023-11-07 12:00:00'): ");
-                String selectedTime = scanner.nextLine();
-                weatherData.setWeatherDataInDatabase(selectedTime);
+
             } else if ("3".equals(choice)) {
                 System.out.print("Enter a new temperature threshold (" + temperatureScale + "): ");
                 double threshold = convertTemperature(Double.parseDouble(scanner.nextLine()), temperatureScale, "Celsius");
                 WeatherObserver temperatureObserver = new WeatherObserver(weatherData, threshold);
-            } else if ("4".equals(choice)) {
-                System.out.print("Choose Data Source (API/Sensor): ");
-                String input = scanner.nextLine();
-                WeatherDataCollectionStrategy dataCollectionStrategy;
-
-                if ("API".equalsIgnoreCase(input)) {
-                    dataCollectionStrategy = new WeatherAPI();
-                } else if ("Sensor".equalsIgnoreCase(input)) {
-                    dataCollectionStrategy = new WeatherSensor();
-                } else {
-                    System.out.println("Invalid choice. Please enter 'API' or 'Sensor'.");
-                    continue;
-                }
-
-                dataCollectionStrategy.collectData(weatherData);
-
-                // After collecting data, add it to the database
-                System.out.print("Enter the time (e.g., '2023-11-07 12:00:00'): ");
-                String selectedTime = scanner.nextLine();
-                weatherData.setWeatherDataInDatabase(selectedTime);
             } else if ("5".equals(choice)) {
-                System.out.println("<==============================================================>");
                 System.out.println("Last Weather Data:");
                 WeatherReportDecorator temperatureDecorator = new TemperatureDecorator(weatherData, temperatureScale);
                 temperatureDecorator.display();
                 System.out.println("Humidity: " + weatherData.getHumidity() + "%");
                 System.out.println("Pressure: " + weatherData.getPressure() + " hPa");
-                System.out.println("<==============================================================>");
             } else if ("6".equals(choice)) {
                 weatherData.displayWeatherCondition();
             } else if ("2".equals(choice)) {
-                System.out.print("Choose Temperature Scale (Celsius/Fahrenheit/Kelvin): ");
-                temperatureScale = scanner.nextLine();
+                    System.out.print("Choose Temperature Scale (Celsius/Fahrenheit/Kelvin): ");
+                    String newScale = scanner.nextLine();
+                    temperatureScaleHistory.push(temperatureScale); // Push the current scale onto the history
+                    temperatureScale = newScale; // Update the current scale
+
             } else if ("7".equals(choice)) {
-                System.out.print("Enter the time (e.g., '2023-11-07 12:00:00'): ");
-                String selectedTime = scanner.nextLine();
-                weatherData.setWeatherDataInDatabase(selectedTime);
-            }   else if ("8".equals(choice)) {
                 System.out.print("Enter a time (e.g., '2023-11-07 12:00:00'): ");
                 String selectedTime = scanner.nextLine();
                 WeatherData retrievedData = weatherData.retrieveDataFromDatabase(selectedTime);
 
                 if (retrievedData.getTemperatureCelsius() != 0) {
-                    System.out.println("<==============================================================>");
                     System.out.println("Retrieved Weather Data:");
                     WeatherReportDecorator temperatureDecorator = new TemperatureDecorator(retrievedData, temperatureScale);
                     temperatureDecorator.display();
                     System.out.println("Humidity: " + retrievedData.getHumidity() + "%");
                     System.out.println("Pressure: " + retrievedData.getPressure() + " hPa");
-                    System.out.println("<==============================================================>");
                 } else {
                     System.out.println("No data found for the specified time.");
+                }
+            } else if ("8".equals(choice)) {
+                // Undo the last operation
+                if (!dataSourceHistory.isEmpty()) {
+                    DataSourceChange previousChange = dataSourceHistory.pop();
+                    dataCollectionStrategy = previousChange.getStrategy();
+
+                    // Restore the state using the memento
+                    WeatherDataMemento memento = previousChange.getMemento();
+                    weatherData.setMeasurements(memento.getTemperatureCelsius(), memento.getHumidity(), memento.getPressure());
+                    weatherData.setConditionState(memento.getConditionState());
+
+                    // Undo the temperature scale change if there is a previous scale in the history
+                    if (!temperatureScaleHistory.isEmpty()) {
+                        temperatureScale = temperatureScaleHistory.pop();
+                    }
+
+                    System.out.println("Undo operation completed.");
+                } else {
+                    System.out.println("No operation to undo.");
                 }
             } else {
                 System.out.println("Invalid choice. Please select a valid option.");
